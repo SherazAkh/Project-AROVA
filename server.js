834b3cdbd5e031cfaf6a09ad43c8ba8e96f93190
Sheraz Akhtar <sherazakh3@gmail.com>
2026-05-05 03:23:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const app = require('./app.js');
-// import { app  from './app.js'
 const dotenv = require('dotenv');
 const path = require('path');
+// Config
+dotenv.config({ path: path.join(__dirname, 'config/config.env') });
 
+const app = require('./app.js');
 const connectDB = require('./db/database');
+const cloudinary = require('cloudinary').v2;
+
+// Cloudinary Configuration
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 process.on('uncaughtException', (err) => {
 	console.log(`Error: ${err.message}`);
 	console.log(`Shutting down server due to Uncaught Promise Rejection`);
 	process.exit(1);
 });
-
-// Config
-dotenv.config({ path: path.join(__dirname, 'config/config.env') });
 
 connectDB();
 const server = app.listen(process.env.PORT || 4000, () => {
